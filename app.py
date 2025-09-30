6100964dfd59f1c29e3376dc386ce429b2d66e14
Harry Best <150065627+harrybest-softwire@users.noreply.github.com>
2025-09-30 16:37:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from azure.monitor.opentelemetry import configure_azure_monitor
+import logging
+
+logging.basicConfig(level=logging.INFO)
+# Configure Azure monitor collection telemetry pipeline
+configure_azure_monitor()
+
+
 from flask import Flask, render_template, request
 from datetime import datetime, timezone
 
@@ -7,8 +15,7 @@ from data.database import initialise_database, add_order, clear_orders, count_or
 from scheduled_jobs import initialise_scheduled_jobs
 from products import create_product_download
 import requests
-import logging
-logging.basicConfig(level=logging.INFO)
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
